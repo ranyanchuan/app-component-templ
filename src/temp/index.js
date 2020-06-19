e586e2247976508a357c2bcf3a38ce8676d7b2c3
ranyanchuan <ranyanchuan@yimeijian.cn>
2020-06-19 08:43:04 +0800

    修改 package.json 里的 git 地址

--- src/temp/index.js
+++ src/temp/index.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
-const propTypes = {
-}
+const propTypes = {}
 
 class Temp extends Component {
 
